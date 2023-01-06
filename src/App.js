6077080bda730e57321b56078fb2c14ceef6baa8
Fred Lefebvre <fredlefevre@outlook.com>
2023-01-06 13:51:26 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import CopyButton from "./CopyButton";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center items-center flex-col">
+      <h1 className="text-2xl text-[#817D92] mb-8">Password Generator</h1>
+      <form className="md:w-[540px] w-[343px]">
+        <div className="relative">
+          <input
+            placeholder="P4$5W0rD!"
+            className="bg-[#24232C] w-full h-20 py-5 pl-7 text-3xl text-[#E6E5EA] focus:outline-none placeholder:opacity-25"
+          />
+          <CopyButton />
+        </div>
+      </form>
     </div>
   );
 }
