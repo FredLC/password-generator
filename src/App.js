849dae6691d2cc106739ddf4130b0c9a5709961b
Fred Lefebvre <fredlefevre@outlook.com>
2023-01-09 17:22:28 -0500

    Implemented character length slider using react-range-slider-input library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import CopyButton from "./CopyButton";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
 
 function App() {
   const [password, setPassword] = useState("");
+  const [characterLength, setCharacterLength] = useState(10);
 
   function handleInputChange(e) {
     setPassword(e.target.value);
+  }
+
+  function handleSliderInputChange(sliderValueArr) {
+    setCharacterLength(sliderValueArr[1]);
   }
 
   return (
@@ -19,6 +26,27 @@ function App() {
             className="bg-[#24232C] w-full h-20 py-5 pl-7 text-3xl text-[#E6E5EA] focus:outline-none placeholder:opacity-25"
           />
           <CopyButton copyText={password} />
+        </div>
+
+        <div className="bg-[#24232C] mt-6 px-8 py-6">
+          <div className="flex justify-between items-center mb-4">
+            <label
+              htmlFor="character-length"
+              className="text-[#E6E5EA] text-base"
+            >
+              Character Length
+            </label>
+            <span className="text-[#A4FFAF] text-3xl">{characterLength}</span>
+          </div>
+          <RangeSlider
+            className="single-thumb"
+            min={1}
+            max={50}
+            defaultValue={[0, 10]}
+            thumbsDisabled={[true, false]}
+            rangeSlideDisabled={true}
+            onInput={handleSliderInputChange}
+          />
         </div>
       </form>
     </div>
